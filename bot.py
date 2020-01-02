a9a25a0f1ca7f1a82f50b5cee6a43dbc03277663
i0dlrve85oe3rw89 <58844704+i0dlrve85oe3rw89@users.noreply.github.com>
2020-01-02 11:46:06 +0300

    фыв

--- bot.py
+++ bot.py
@@ -9,7 +9,11 @@ bot = telebot.TeleBot('1012369969:AAHk2zKjTv6m_gvA7bEYQWN-aiDn2xd9UbU')
 
 @bot.message_handler(commands=['start'])
 def send_krol_cool(message):
-    bot.send_message(message.chat.id, 'hi.')
+    bot.send_message(message.chat.id, 'Кролечка - крутая девочка, солнышко + заечка. Я все сказал.')
+    print(u"Пользователь @" + message.chat.username + u" написал - " + message.text)
 
-
+@bot.message_handler(content_types=['text'])
+def send_krol_cool_text(message):
+    print(u"Пользователь @" + message.chat.username + u" написал - " + message.text)
 bot.polling()
+
